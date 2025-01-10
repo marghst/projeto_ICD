9a215975f8172c67509361b2e5d6f7312b2c61a5
Margarida Santos <margaridahst@gmail.com>
2025-01-10 13:43:06 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,276 +1,232 @@
-#######################
-# Import libraries
 import streamlit as st
 import pandas as pd
-import altair as alt
 import plotly.express as px
+import plotly.graph_objects as go
+import plotly.colors as pc
 
-#######################
-# Page configuration
+# URL do VOSviewer
+url = "https://tinyurl.com/233s5fwm"
+
+# Carregar os dados para o gráfico de barras
+termos_titulos = pd.read_csv('dados_streamlit/termos_titulos.csv')
+
+# Definir as cores customizadas
+custom_colors = [
+    "#003f5b", "#2b4b7d", "#5f5195", "#98509d", 
+    "#cc4c91", "#f25375", "#ff6f4e", "#ff9913"]
+
+# Criar o gráfico de barras interativo para visualizar os 10 primeiros termos
+fig = px.bar(
+    termos_titulos.head(10),
+    x='Count',
+    y='Term',
+    orientation='h',
+    labels={'Count': 'Frequência', 'Term': 'Termo'},
+    color='Count',
+    color_continuous_scale=custom_colors
+)
+
+# Ajustar o layout do gráfico
+fig.update_layout(
+    xaxis_title='Frequência',
+    yaxis_title='Termos',
+    yaxis=dict(autorange='reversed'),  # Inverter a ordem dos termos no eixo y
+    height=500,  # Aumentar a altura do gráfico
+    margin=dict(l=30, r=30, t=40, b=40)  # Ajustar margens para dar mais espaço
+)
+
+# Layout do Streamlit
 st.set_page_config(
-    page_title="US Population Dashboard",
-    page_icon="🏂",
+    page_title="Dashboard - Usos de Data Science e Big Data no Planeamento Urbano",
+    page_icon="📊",
     layout="wide",
-    initial_sidebar_state="expanded")
+    initial_sidebar_state="expanded"
+)
 
-alt.themes.enable("dark")
+df_all = pd.read_csv('/workspaces/projeto_ICD/dados_streamlit/df_all_geral.csv')
+# Supondo que 'df_all' contenha os dados com as colunas 'year', 'affiliation-country', e 'count'
+df_all['count'] = 1  # Inicializar com 1 para representar cada artigo
+df_all = df_all.groupby(['year', 'affiliation-country'], as_index=False).agg({'count': 'sum'})
 
-#######################
-# CSS styling
+# Encontrar o valor máximo de 'count' em qualquer país ao longo de todos os anos
+max_count = df_all['count'].max()
+
+# Criar o mapa interativo
+fig1 = px.choropleth(
+    df_all,
+    locations="affiliation-country",  # Nome do país
+    locationmode="country names",  # Usando nomes de países
+    color="count",  # Usando a coluna 'count' para a cor
+    color_continuous_scale=custom_colors,
+    hover_name="affiliation-country",  # Para exibir o nome do país
+    animation_frame="year",  # Para animar o mapa ao longo dos anos
+    range_color=[0, max_count]  # Define a escala de cor de 0 até o valor máximo fixo
+)
+
+# Ajustar o layout do mapa com fundo preto
+fig1.update_layout(
+    geo=dict(
+        showframe=False, 
+        showcoastlines=True, 
+        projection_type='natural earth',
+        bgcolor="black"  # Define o fundo do mapa como preto
+    ),
+    coloraxis_colorbar=dict(title="Número de Artigos"),
+    paper_bgcolor="black",  # Fundo da área de papel também preto
+    plot_bgcolor="black"    # Fundo do gráfico também preto
+)
+
+df_authors = pd.read_csv('/workspaces/projeto_ICD/dados_streamlit/df_authors.csv')
+# Definir as colunas obrigatórias
+# Definição das colunas e dados (ajustar conforme necessário)
+required_columns = ['author', 'n_artigos_pub', 'affiliation', 'country']
+if not all(column in df_authors.columns for column in required_columns):
+    raise ValueError(f"O DataFrame deve conter as colunas: {required_columns}")
+
+# Criar uma coluna com as siglas das instituições
+df_authors['institution_labels_simp'] = df_authors['affiliation'].apply(lambda x: ''.join([word[0].upper() for word in x.split()]))
+
+# Selecionar os 10 autores com mais publicações
+top_authors_df = df_authors.nlargest(10, 'n_artigos_pub')
+
+# Preparar os dados para o diagrama de Sankey
+authors = top_authors_df['author'].tolist()
+affiliations = top_authors_df['affiliation'].tolist()
+countries = top_authors_df['country'].dropna().unique().tolist()
+institution_labels_simp = [df_authors[df_authors['affiliation'] == inst]['institution_labels_simp'].values[0] for inst in affiliations]
+
+# Criar labels para países, instituições (simplificadas) e top 10 autores
+labels = countries + institution_labels_simp + authors
+
+# Criar dicionários para mapear os índices
+country_indices = {country: i for i, country in enumerate(countries)}
+affiliation_indices = {affiliation: i + len(countries) for i, affiliation in enumerate(institution_labels_simp)}
+author_indices = {author: i + len(countries) + len(institution_labels_simp) for i, author in enumerate(authors)}
+
+# Criar listas para as fontes e destinos
+sources = []
+targets = []
+values = []
+link_colors = []
+
+# Definir cores para os países
+custom_colors = [
+    "#5f5195", "#98509d", 
+    "#cc4c91", "#f25375",
+    "#ff6f4e", "#ff9913"
+]
+country_colors = custom_colors[:len(countries)]
+
+# Adicionar relações país -> instituição
+for i, row in top_authors_df.iterrows():
+    sources.append(country_indices[row['country']])
+    targets.append(affiliation_indices[institution_labels_simp[affiliations.index(row['affiliation'])]])
+    values.append(row['n_artigos_pub'])
+    link_colors.append(country_colors[countries.index(row['country'])])
+
+# Adicionar relações instituição -> autor
+for i, row in top_authors_df.iterrows():
+    sources.append(affiliation_indices[institution_labels_simp[affiliations.index(row['affiliation'])]])
+    targets.append(author_indices[row['author']])
+    values.append(row['n_artigos_pub'])
+    # Criar uma cor mais clara para o autor
+    base_color = pc.hex_to_rgb(country_colors[countries.index(row['country'])])
+    lighter_color = pc.find_intermediate_color(base_color, (255, 255, 255), 0.5)
+    lighter_color = f"rgb{lighter_color}"
+    link_colors.append(lighter_color)
+
+# Criar customdata para incluir o nome completo e o número de artigos
+customdata = []
+for label in labels:
+    if label in authors:
+        count = top_authors_df[top_authors_df['author'] == label]['n_artigos_pub'].values[0]
+        customdata.append(f"{label}<br>Number of articles: {count}")
+    elif label in institution_labels_simp:
+        full_name = affiliations[institution_labels_simp.index(label)]
+        count = top_authors_df[top_authors_df['affiliation'] == full_name]['n_artigos_pub'].sum()
+        customdata.append(f"{full_name}<br>Number of articles: {count}")
+    else:
+        count = top_authors_df[top_authors_df['country'] == label]['n_artigos_pub'].sum()
+        customdata.append(f"{label}<br>Number of articles: {count}")
+
+# Criar o diagrama de Sankey
+fig2 = go.Figure(data=[go.Sankey(
+    node=dict(
+        pad=20,
+        thickness=20,  # Espessura dos nós ajustada
+        line=dict(color="white", width=0),  # Remover o contorno dos nós (sem outline)
+        label=labels,
+        customdata=customdata,  # Texto completo para o pop-up
+        hovertemplate='%{customdata}<extra></extra>',  # Formato do pop-up
+        color="#003f5c",  # Cor dos nós ajustada para cinza
+    ),
+    link=dict(
+        source=sources,
+        target=targets,
+        value=values,
+        color=link_colors
+    )
+)])
+
+# Ajuste do layout para tornar o gráfico mais clean e bonito
+fig2.update_layout(
+    hovermode='x',
+    font=dict(size=14, color='Black', family="Arial"),  # Aumentar o tamanho da fonte
+    plot_bgcolor='black',  # Fundo preto
+    paper_bgcolor='black',  # Fundo da página também preto
+    width=700,  # Largura do gráfico mais ajustada
+    height=300,  # Altura ajustada para um aspecto mais equilibrado
+    showlegend=False  # Remover a legenda para não sobrecarregar o gráfico
+)
+
+
+# CSS styling para o fundo da rede
 st.markdown("""
+<div style="background-color: black; padding: 20px; border-radius: 5px; text-align: left;">
+    <h1 style="font-size: 40px; font-weight: bold; color: #bc5090;">Análise de Artigos Científicos da área das Ciências Sociais</h1>
+    <h2 style="font-size: 32px; font-weight: normal; color: #ffa600;">Data Science e Big Data, aplicadas ao Planeamento Urbano</h2>
+</div>
+
 <style>
-
-[data-testid="block-container"] {
-    padding-left: 2rem;
-    padding-right: 2rem;
-    padding-top: 1rem;
-    padding-bottom: 0rem;
-    margin-bottom: -7rem;
+/* Estilo do iframe da rede com fundo preto */
+iframe {
+    background-color: black;
 }
-
-[data-testid="stVerticalBlock"] {
-    padding-left: 0rem;
-    padding-right: 0rem;
-}
-
-[data-testid="stMetric"] {
-    background-color: #393939;
-    text-align: center;
-    padding: 15px 0;
-}
-
-[data-testid="stMetricLabel"] {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-[data-testid="stMetricDeltaIcon-Up"] {
-    position: relative;
-    left: 38%;
-    -webkit-transform: translateX(-50%);
-    -ms-transform: translateX(-50%);
-    transform: translateX(-50%);
-}
-
-[data-testid="stMetricDeltaIcon-Down"] {
-    position: relative;
-    left: 38%;
-    -webkit-transform: translateX(-50%);
-    -ms-transform: translateX(-50%);
-    transform: translateX(-50%);
-}
-
 </style>
 """, unsafe_allow_html=True)
 
 
-#######################
-# Load data
-df_reshaped = pd.read_csv('data/us-population-2010-2019-reshaped.csv')
+# Criar a aba
+tab1, tab2 = st.tabs([" 📈 Análise Bibliográfica", " 📊 Análise Bibliométrica"])
 
+# Aba 1: Análise Bibliográfica
+with tab1:
+    st.markdown("""
+<div style="background-color: #333; padding: 10px; border-radius: 5px; color: white; font-size: 16px; margin-bottom: 20px;">
+    Query da pesquisa SCOPUS: Data Science OR Big Data AND (Urban Planning OR Urban Management OR Spatial Planning OR Urban Development), limited to Social Sciences
+</div>
+""", unsafe_allow_html=True)
 
-#######################
-# Sidebar
-with st.sidebar:
-    st.title('🏂 US Population Dashboard')
-    
-    year_list = list(df_reshaped.year.unique())[::-1]
-    
-    selected_year = st.selectbox('Select a year', year_list)
-    df_selected_year = df_reshaped[df_reshaped.year == selected_year]
-    df_selected_year_sorted = df_selected_year.sort_values(by="population", ascending=False)
+    # Dividir a tela em três colunas, conforme exemplo fornecido
+    col = st.columns((5, 5), gap='medium')
 
-    color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
-    selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
+    with col[0]:
+        st.markdown('#### Rede bibliométrica de co-ocorrência de keywords dos autores')
+        # Incorporar o conteúdo do VOSviewer diretamente com fundo preto
+        st.components.v1.html(f'<iframe src="{url}" width="100%" height="600px" style="border: 0px solid #ddd; max-width: 1000px; min-height: 500px; background-color: black;"></iframe>', height=600)
+        st.markdown('### País de publicação dos artigos')
+        st.plotly_chart(fig1, use_container_width=True)
 
+    with col[1]:
+        st.markdown('#### Top 10: Termos mais recorrentes nos títulos dos artigos')
+        # Exibir o gráfico de barras
+        st.plotly_chart(fig, use_container_width=True)
+        st.markdown('###   ')
+        st.markdown('###   ')
+        st.markdown('### TOP 10 de Autores, por nº de publicações')
+        st.plotly_chart(fig2)
 
-#######################
-# Plots
-
-# Heatmap
-def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
-    heatmap = alt.Chart(input_df).mark_rect().encode(
-            y=alt.Y(f'{input_y}:O', axis=alt.Axis(title="Year", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
-            x=alt.X(f'{input_x}:O', axis=alt.Axis(title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
-            color=alt.Color(f'max({input_color}):Q',
-                             legend=None,
-                             scale=alt.Scale(scheme=input_color_theme)),
-            stroke=alt.value('black'),
-            strokeWidth=alt.value(0.25),
-        ).properties(width=900
-        ).configure_axis(
-        labelFontSize=12,
-        titleFontSize=12
-        ) 
-    # height=300
-    return heatmap
-
-# Choropleth map
-def make_choropleth(input_df, input_id, input_column, input_color_theme):
-    choropleth = px.choropleth(input_df, locations=input_id, color=input_column, locationmode="USA-states",
-                               color_continuous_scale=input_color_theme,
-                               range_color=(0, max(df_selected_year.population)),
-                               scope="usa",
-                               labels={'population':'Population'}
-                              )
-    choropleth.update_layout(
-        template='plotly_dark',
-        plot_bgcolor='rgba(0, 0, 0, 0)',
-        paper_bgcolor='rgba(0, 0, 0, 0)',
-        margin=dict(l=0, r=0, t=0, b=0),
-        height=350
-    )
-    return choropleth
-
-
-# Donut chart
-def make_donut(input_response, input_text, input_color):
-  if input_color == 'blue':
-      chart_color = ['#29b5e8', '#155F7A']
-  if input_color == 'green':
-      chart_color = ['#27AE60', '#12783D']
-  if input_color == 'orange':
-      chart_color = ['#F39C12', '#875A12']
-  if input_color == 'red':
-      chart_color = ['#E74C3C', '#781F16']
-    
-  source = pd.DataFrame({
-      "Topic": ['', input_text],
-      "% value": [100-input_response, input_response]
-  })
-  source_bg = pd.DataFrame({
-      "Topic": ['', input_text],
-      "% value": [100, 0]
-  })
-    
-  plot = alt.Chart(source).mark_arc(innerRadius=45, cornerRadius=25).encode(
-      theta="% value",
-      color= alt.Color("Topic:N",
-                      scale=alt.Scale(
-                          #domain=['A', 'B'],
-                          domain=[input_text, ''],
-                          # range=['#29b5e8', '#155F7A']),  # 31333F
-                          range=chart_color),
-                      legend=None),
-  ).properties(width=130, height=130)
-    
-  text = plot.mark_text(align='center', color="#29b5e8", font="Lato", fontSize=32, fontWeight=700, fontStyle="italic").encode(text=alt.value(f'{input_response} %'))
-  plot_bg = alt.Chart(source_bg).mark_arc(innerRadius=45, cornerRadius=20).encode(
-      theta="% value",
-      color= alt.Color("Topic:N",
-                      scale=alt.Scale(
-                          # domain=['A', 'B'],
-                          domain=[input_text, ''],
-                          range=chart_color),  # 31333F
-                      legend=None),
-  ).properties(width=130, height=130)
-  return plot_bg + plot + text
-
-# Convert population to text 
-def format_number(num):
-    if num > 1000000:
-        if not num % 1000000:
-            return f'{num // 1000000} M'
-        return f'{round(num / 1000000, 1)} M'
-    return f'{num // 1000} K'
-
-# Calculation year-over-year population migrations
-def calculate_population_difference(input_df, input_year):
-  selected_year_data = input_df[input_df['year'] == input_year].reset_index()
-  previous_year_data = input_df[input_df['year'] == input_year - 1].reset_index()
-  selected_year_data['population_difference'] = selected_year_data.population.sub(previous_year_data.population, fill_value=0)
-  return pd.concat([selected_year_data.states, selected_year_data.id, selected_year_data.population, selected_year_data.population_difference], axis=1).sort_values(by="population_difference", ascending=False)
-
-
-#######################
-# Dashboard Main Panel
-col = st.columns((1.5, 4.5, 2), gap='medium')
-
-with col[0]:
-    st.markdown('#### Gains/Losses')
-
-    df_population_difference_sorted = calculate_population_difference(df_reshaped, selected_year)
-
-    if selected_year > 2010:
-        first_state_name = df_population_difference_sorted.states.iloc[0]
-        first_state_population = format_number(df_population_difference_sorted.population.iloc[0])
-        first_state_delta = format_number(df_population_difference_sorted.population_difference.iloc[0])
-    else:
-        first_state_name = '-'
-        first_state_population = '-'
-        first_state_delta = ''
-    st.metric(label=first_state_name, value=first_state_population, delta=first_state_delta)
-
-    if selected_year > 2010:
-        last_state_name = df_population_difference_sorted.states.iloc[-1]
-        last_state_population = format_number(df_population_difference_sorted.population.iloc[-1])   
-        last_state_delta = format_number(df_population_difference_sorted.population_difference.iloc[-1])   
-    else:
-        last_state_name = '-'
-        last_state_population = '-'
-        last_state_delta = ''
-    st.metric(label=last_state_name, value=last_state_population, delta=last_state_delta)
-
-    
-    st.markdown('#### States Migration')
-
-    if selected_year > 2010:
-        # Filter states with population difference > 50000
-        # df_greater_50000 = df_population_difference_sorted[df_population_difference_sorted.population_difference_absolute > 50000]
-        df_greater_50000 = df_population_difference_sorted[df_population_difference_sorted.population_difference > 50000]
-        df_less_50000 = df_population_difference_sorted[df_population_difference_sorted.population_difference < -50000]
-        
-        # % of States with population difference > 50000
-        states_migration_greater = round((len(df_greater_50000)/df_population_difference_sorted.states.nunique())*100)
-        states_migration_less = round((len(df_less_50000)/df_population_difference_sorted.states.nunique())*100)
-        donut_chart_greater = make_donut(states_migration_greater, 'Inbound Migration', 'green')
-        donut_chart_less = make_donut(states_migration_less, 'Outbound Migration', 'red')
-    else:
-        states_migration_greater = 0
-        states_migration_less = 0
-        donut_chart_greater = make_donut(states_migration_greater, 'Inbound Migration', 'green')
-        donut_chart_less = make_donut(states_migration_less, 'Outbound Migration', 'red')
-
-    migrations_col = st.columns((0.2, 1, 0.2))
-    with migrations_col[1]:
-        st.write('Inbound')
-        st.altair_chart(donut_chart_greater)
-        st.write('Outbound')
-        st.altair_chart(donut_chart_less)
-
-with col[1]:
-    st.markdown('#### Total Population')
-    
-    choropleth = make_choropleth(df_selected_year, 'states_code', 'population', selected_color_theme)
-    st.plotly_chart(choropleth, use_container_width=True)
-    
-    heatmap = make_heatmap(df_reshaped, 'year', 'states', 'population', selected_color_theme)
-    st.altair_chart(heatmap, use_container_width=True)
-    
-
-with col[2]:
-    st.markdown('#### Top States')
-
-    st.dataframe(df_selected_year_sorted,
-                 column_order=("states", "population"),
-                 hide_index=True,
-                 width=None,
-                 column_config={
-                    "states": st.column_config.TextColumn(
-                        "States",
-                    ),
-                    "population": st.column_config.ProgressColumn(
-                        "Population",
-                        format="%f",
-                        min_value=0,
-                        max_value=max(df_selected_year_sorted.population),
-                     )}
-                 )
-    
-    with st.expander('About', expanded=True):
-        st.write('''
-            - Data: [U.S. Census Bureau](https://www.census.gov/data/datasets/time-series/demo/popest/2010s-state-total.html).
-            - :orange[**Gains/Losses**]: states with high inbound/ outbound migration for selected year
-            - :orange[**States Migration**]: percentage of states with annual inbound/ outbound migration > 50,000
-            ''')
+# Aba 2: Análise Bibliométrica
+with tab2:
+    st.title("📈 Análise Bibliométrica")
+    st.write("Nesta aba, apresentamos métricas e análises bibliométricas com base nos artigos selecionados.")
